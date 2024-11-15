f149534ba32ede127c2a0dbddb9fc20349c8cf9e
labstraction <45163263+labstraction@users.noreply.github.com>
2024-11-15 10:36:12 +0100

    pubblicazione

--- programma.js
+++ programma.js
@@ -11,3 +11,12 @@ disegno.fillRect(0,0, 100, 50)
 disegno.fillStyle = 'green'
 
 disegno.fillRect(30, 30, 100, 50)
+
+
+let foglio2 = document.getElementById('foglio2');
+
+let disegno2 = foglio2.getContext('2d');
+
+foglio2.addEventListener('click', (evento) => {
+    disegno2.strokeRect(evento.offsetX, evento.offsetY, 500 * Math.random(), 500 * Math.random());
+});
